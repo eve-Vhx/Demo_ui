3b9b78c1ecd539148369e48658f293c719be4e41
nico-eve <nico@evevehicles.com>
2022-12-19 17:17:16 -0600

    changed buttons into sliders for the gimbal's yaw and pitch

--- src/components/GimbalVis.jsx
+++ src/components/GimbalVis.jsx
@@ -6,13 +6,13 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Col  from 'react-bootstrap/Col';
 import Container  from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+// import RangeSlider from 'react-bootstrap/RangeSlider';
 import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
 import Row  from 'react-bootstrap/Row';
 import Stack from 'react-bootstrap/Stack';
-
 import '../css/Telem.css';
 import { gimbal_publisher, ROSLIB } from './RosCon';
-
+import RangeSlider from 'react-bootstrap-range-slider';
 
 
 function TelemVis() {
@@ -20,76 +20,54 @@ function TelemVis() {
     var yaw_deg = 0;
     var pitch_deg = 0;
     var roll_deg = 0;
+    var ctrl_mode = 2;
 
-    function MoveGimbalLeft() {
-      if(yaw_deg-5 > -180){
-        yaw_deg = yaw_deg - 10;
-      }
-
+    const [ value1, setValue1 ] = useState(0);
+    const [ value2, setValue2 ] = useState(0);
+    
+    function UpdateYaw(event){
+      setValue1(event.target.value);
+      yaw_deg = event.target.value;
+      var gimbal_msg = new ROSLIB.Message({
+        yaw: Number(yaw_deg),
+        pitch: pitch_deg,
+        roll: roll_deg,
+        mode: ctrl_mode
+      });
+      console.log(gimbal_msg);
+      gimbal_publisher.publish(gimbal_msg);
+    }
+    function UpdatePitch(event){
+      setValue2(event.target.value);
+      pitch_deg = event.target.value;
       var gimbal_msg = new ROSLIB.Message({
         yaw: yaw_deg,
-        pitch: pitch_deg,
-        roll: roll_deg
+        pitch: Number(pitch_deg),
+        roll: roll_deg,
+        mode: ctrl_mode
       });
       gimbal_publisher.publish(gimbal_msg);
-
     }
-
-    function MoveGimbalRight() {
-      if(yaw_deg+5 < 180){
-        yaw_deg = yaw_deg + 10;
-      }
-
-      var gimbal_msg = new ROSLIB.Message({
-        yaw: yaw_deg,
-        pitch: pitch_deg,
-        roll: roll_deg
-      });
-      gimbal_publisher.publish(gimbal_msg);
-
-    }
-
-    function MoveGimbalUp() {
-      if(pitch_deg+5 < 25){
-        pitch_deg = pitch_deg + 10;
-      }
-
-      var gimbal_msg = new ROSLIB.Message({
-        yaw: yaw_deg,
-        pitch: pitch_deg,
-        roll: roll_deg
-      });
-      gimbal_publisher.publish(gimbal_msg);
-
-    }
-
-    function MoveGimbalDown() {
-      if(pitch_deg-5 > -25){
-        pitch_deg = pitch_deg - 10;
-      }
-
-      var gimbal_msg = new ROSLIB.Message({
-        yaw: yaw_deg,
-        pitch: pitch_deg,
-        roll: roll_deg
-      });
-      gimbal_publisher.publish(gimbal_msg);
-
-    }
-
     return(
       <>
       <Col>
-        <ButtonGroup>
-          <Button variant='secondary' onClick={MoveGimbalLeft}>Left</Button>
-          <Button variant='secondary' onClick={MoveGimbalRight}>Right</Button>
-        </ButtonGroup>
+      Yaw
+      <RangeSlider
+            name="Yaw"
+            value={value1}
+            min={-180}
+            max={180}
+            onChange={UpdateYaw} 
+    />
       </Col>
       <Col>
-        <ButtonGroup>
-          <Button variant='secondary' onClick={MoveGimbalUp}>Up</Button>
-          <Button variant='secondary' onClick={MoveGimbalDown}>Down</Button>
-        </ButtonGroup>
+      Pitch
+      <RangeSlider
+            value={value2}
+            min={-30}
+            max={30}
+            onChange={UpdatePitch}
+          />
       </Col>
       </>
     )
